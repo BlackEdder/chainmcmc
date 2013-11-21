11dc3056d33c2e665e06d23edb989f6c4bd7897b
BlackEdder <edwin@tkwsping.nl>
2013-11-21 10:28:45 +0000

    Started work on adding default priors

---
+++ include/chainmcmc/prior.hh
@@ -0,0 +1,31 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2013, Edwin van Leeuwen
+  
+  This file is part of chainmcmc.
+  
+ 	chainmcmc is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  chainmcmc is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with chainmcmc. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+
+#ifndef PRIOR_HH 
+#define PRIOR_HH
+
+namespace chainmcmc {
+	namespace prior {
+	};
+};
+#endif
